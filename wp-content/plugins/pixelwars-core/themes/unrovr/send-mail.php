aea9e221d4b33f043ea3dcbbc5a45d533ae8314a
agung <adeagungkp@gmail.com>
2019-07-31 01:26:19 +0700

    after template come
    after

--- wp-content/plugins/pixelwars-core/themes/unrovr/send-mail.php
+++ wp-content/plugins/pixelwars-core/themes/unrovr/send-mail.php
@@ -1,78 +1,29 @@
-<?php
-
-	if (! defined(ABSPATH))
-	{
-		$pagePath = explode('/wp-content/', dirname(__FILE__));
-		include_once(str_replace('wp-content/', '', $pagePath[0] . '/wp-load.php'));
-	}
-
-
-/* ============================================================================================================================================= */
-
-
-	// site owner
-
-	$site_name = get_bloginfo( 'name' );
-	$sender_domain = trim( $_POST['sender_domain'] );
-	$admin_email = get_bloginfo( 'admin_email' );
-	$to = get_option( 'contact_form_to', $admin_email );
-	$subject = trim( $_POST['subject'] );
-
-
-/* ============================================================================================================================================= */
-
-
-	// contact form fields
-	
-	$name = trim( $_POST['name'] );
-	$email = trim( $_POST['email'] );
-	$message = trim( $_POST['message'] );
-	$url = trim( $_POST['url'] );
-
-
-/* ============================================================================================================================================= */
-
-
-	// check for errors
-	
-	$error = false;
-	
-	if ( $name === "" ) { $error = true; }
-	elseif ( $email === "" ) { $error = true; }
-	elseif ( $message === "" ) { $error = true; }
-	elseif ( $to === "" ) { $error = true; }
-
-
-/* ============================================================================================================================================= */
-
-
-	// send mail
-	
-	if ( isset( $url ) && $url == "" )
-	{
-		if ( $error == false )
-		{
-			$body = "Name: $name" . "\n\n" . "Email: $email" . "\n\n" . "Message: $message";
-			
-			$headers = "From: " . $site_name . ' <' . $sender_domain . '> ' . "\r\n";
-			$headers .= "Reply-To: " . $name . ' <' . $email . '> ' . "\r\n";
-			
-			
-			$mail_result = mail( $to, $subject, $body, $headers );
-			
-			if ( $mail_result == true )
-			{
-				echo 'success';
-			}
-			else
-			{
-				echo 'unsuccess';
-			}
-		}
-		else
-		{
-			echo 'error';
-		}
-	}
-
-?>
+<br />
+<font size='1'><table class='xdebug-error xe-warning' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Warning: Use of undefined constant ABSPATH - assumed 'ABSPATH' (this will throw an Error in a future version of PHP) in C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php on line <i>3</i></th></tr>
+<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
+<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0043</td><td bgcolor='#eeeeec' align='right'>401344</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php' bgcolor='#eeeeec'>...\send-mail.php<b>:</b>0</td></tr>
+</table></font>
+<br />
+<font size='1'><table class='xdebug-error xe-warning' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Warning: include_once(C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr/wp-load.php): failed to open stream: No such file or directory in C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php on line <i>6</i></th></tr>
+<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
+<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0043</td><td bgcolor='#eeeeec' align='right'>401344</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php' bgcolor='#eeeeec'>...\send-mail.php<b>:</b>0</td></tr>
+</table></font>
+<br />
+<font size='1'><table class='xdebug-error xe-warning' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Warning: include_once(): Failed opening 'C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr/wp-load.php' for inclusion (include_path='.;C:\php\pear') in C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php on line <i>6</i></th></tr>
+<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
+<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0043</td><td bgcolor='#eeeeec' align='right'>401344</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php' bgcolor='#eeeeec'>...\send-mail.php<b>:</b>0</td></tr>
+</table></font>
+<br />
+<font size='1'><table class='xdebug-error xe-uncaught-exception' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Fatal error: Uncaught Error: Call to undefined function get_bloginfo() in C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php on line <i>15</i></th></tr>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Error: Call to undefined function get_bloginfo() in C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php on line <i>15</i></th></tr>
+<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
+<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0043</td><td bgcolor='#eeeeec' align='right'>401344</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\wamp64\www\aakp\wp-content\plugins\pixelwars-core\themes\unrovr\send-mail.php' bgcolor='#eeeeec'>...\send-mail.php<b>:</b>0</td></tr>
+</table></font>
